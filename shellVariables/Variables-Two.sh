aa71d988df0196303d1ebd5674e1452bc91cef57
unknown <Hemanthgurujupalli@gmail.com>
2026-02-26 05:09:47 +0530

    Action

--- shellVariables/Variables-Two.sh
+++ shellVariables/Variables-Two.sh
@@ -2,5 +2,5 @@
 PERSONONE=Suresh
 PERSONTWO=Ramesh
 
-echo = $PERSONONE : "Hi"" $PERSONTWO "How are you"
+echo = $PERSONONE : "Hi" $PERSONTWO "How are you"
 echo = $PERSONTWO : "Hi" $PERSONONE ", Iam Fine" 
