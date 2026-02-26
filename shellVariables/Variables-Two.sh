e5d0e594702fcaabc947f6ca487b33cbbc7f8677
unknown <Hemanthgurujupalli@gmail.com>
2026-02-26 05:56:46 +0530

    Action

--- shellVariables/Variables-Two.sh
+++ shellVariables/Variables-Two.sh
@@ -12,7 +12,7 @@ echo $personTwo :::: Hi $personOne , Iam Fine $personOne
 echo Please Enter Your UserName 
 read user_name
 echo Please Enter Your Password
-read user_password 
+read -s user_password 
 echo Your User Name That You Given Is $user_name
 echo Your User Password That You Given Is $user_password
 echo Script Start Time $start_Time
