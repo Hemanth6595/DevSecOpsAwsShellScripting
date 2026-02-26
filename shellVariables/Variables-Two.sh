7439cf8a9dbbe280a1d3dc397950565bc3382e8f
unknown <Hemanthgurujupalli@gmail.com>
2026-02-26 05:47:37 +0530

    Action

--- shellVariables/Variables-Two.sh
+++ shellVariables/Variables-Two.sh
@@ -1,6 +1,6 @@
 #! bin/bash
-PERSONONE=$1
-PERSONTWO=$2
+personOne=$1
+personTwo=$2
 
 
 
@@ -8,9 +8,9 @@ echo $PERSONONE :::: Hi $PERSONTWO How are you $PERSONTWO
 echo $PERSONTWO :::: Hi $PERSONONE , Iam Fine $PERSONONE
 
 echo Please Enter Your UserName 
-read USER_NAME
+read user_name
 echo Please Enter Your Password
-read USER_PASSWORD 
+read user_password 
 echo Your User Name That You Given Is $USER_NAME
 echo Your User Password That You Given Is $USER_PASSWORD
 
