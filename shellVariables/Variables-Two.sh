34c5ae3961f56a63ece456832676ce9cd5435954
unknown <Hemanthgurujupalli@gmail.com>
2026-02-26 05:33:40 +0530

    Action

--- shellVariables/Variables-Two.sh
+++ shellVariables/Variables-Two.sh
@@ -3,10 +3,11 @@ PERSONONE=$1
 PERSONTWO=$2
 
 
-echo = $PERSONONE :::: Hi $PERSONTWO How are you $PERSONTWO
-echo = $PERSONTWO :::: Hi $PERSONONE , Iam Fine $PERSONONE
+echo $PERSONONE :::: Hi $PERSONTWO How are you $PERSONTWO
+echo $PERSONTWO :::: Hi $PERSONONE , Iam Fine $PERSONONE
 
-echo = "Please Enter Your UserName"
-read -SUSER_NAME
-echo = Your User Name Is -SUSER_NAME
+echo Please Enter Your UserName
+read -S  USER_NAME
+echo Your User Name Is -SUSER_NAME
+
 
